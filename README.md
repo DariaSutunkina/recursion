d0a59fed1517807672a52f3d65bd423a7051411e
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-02-02 20:31:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## recursion
+
+Write a program that calculates the factorial of a number using recursion
---
+++ src/edu/dashasutunkina/recursion.java
@@ -0,0 +1,17 @@
+package edu.dashasutunkina;
+
+public class recursion {
+    public static void main(String[] args) {
+        int value =5;
+        int result = factorial (value);
+        System.out.println(result);
+    }
+    private static int factorial(int value){
+        if (value == 1) {
+            return 1;
+        }
+        return value * factorial( value - 1);
+    }
+}
+
+
